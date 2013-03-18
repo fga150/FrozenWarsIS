20bc6a389f758be8d5429b832124d55cc8f2a622
gonzalopericacho <gonzalopericacho@gmail.com>
2013-03-18 20:02:43 +0100

    disconnect event updated 2
    now if the player is gaming dsn't search the queues etc

--- MyExt/src/com/a51integrated/sfs2x/Disconnect.java
+++ MyExt/src/com/a51integrated/sfs2x/Disconnect.java
@@ -33,102 +33,107 @@ public class Disconnect extends BaseServerEventHandler {
 		List<Room> rooms= (List<Room>) event.getParameter(SFSEventParam.JOINED_ROOMS); // list of all the rooms he was joinend
 		Iterator<Room> itrooms=rooms.iterator();
 		Room room= itrooms.next();
-		if (room.getName()!="The Lobby"){// send a message to all the players he was playing with
-			
+		if (!room.getName().equals("The Lobby")){// send a message to all the players he was playing with
 			List<User> list=room.getUserList(); // list with all the players of the room he was joined
 			Iterator<User> iterator=list.iterator();
 			ISFSObject rtn = new SFSObject();
-			rtn.putUtfString("res", player.getName()+" got disconnected");
-			while (iterator.hasNext()){
-				parentEx.send("meter1", rtn, iterator.next());
+			int playerid=1;
+			while (room.getUserByPlayerId(playerid)!=null){
+				playerid++;
 			}
-		}
-		if(gamesInCreation.containsKey(player.getName())){
-			 ISFSArray accplayer = gamesInCreation.get(player.getName()).getAcceptedPlayers();
-		        for (int j=0; j<accplayer.size();j++){ //Sends the response to the joinned players.
-		        	if(accplayer.getUtfString(j) != player.getName())
-		        	parentEx.send("LeaderLeft", null, users.get(accplayer.getUtfString(j)));
-		        }
-		        
-		        gamesInCreation.remove(player.getName());
-			
+			rtn.putInt("id", playerid);
+			while (iterator.hasNext()){
+				parentEx.send("DisconectedOnGame", rtn, iterator.next());			
+			}
 		}
 		else{
-			@SuppressWarnings("rawtypes")
-			Set set = gamesInCreation.entrySet();
-
-			@SuppressWarnings("rawtypes")
-			Iterator iter = set.iterator();
-			boolean found = false;
-			while (iter.hasNext() && !found) { //Gets the keys of the hashmap. This keys are the user names
-			@SuppressWarnings({ "rawtypes" })
-			Map.Entry entry = (Map.Entry) iter.next();
-			if(gamesInCreation.get(entry.getKey()).getAcceptedPlayers().contains(player.getName())){
-				gamesInCreation.get(entry.getKey()).putRefused(player.getName());
+			if(gamesInCreation.containsKey(player.getName())){
+				 ISFSArray accplayer = gamesInCreation.get(player.getName()).getAcceptedPlayers();
+			        for (int j=0; j<accplayer.size();j++){ //Sends the response to the joinned players.
+			        	if(accplayer.getUtfString(j) != player.getName())
+			        	parentEx.send("LeaderLeft", null, users.get(accplayer.getUtfString(j)));
+			        }
+			        
+			        gamesInCreation.remove(player.getName());
 				
-				ISFSArray accepted = new SFSArray();
-				 accepted = gamesInCreation.get(entry.getKey()).getAcceptedPlayers();
-				
-				ISFSArray refused = new SFSArray();
-				 refused = gamesInCreation.get(entry.getKey()).getRefusedPlayers();
-				
-		       ISFSObject rtn = new SFSObject();
-		       rtn.putSFSArray("acceptedPlayers", accepted);
-		       rtn.putSFSArray("refusedPlayers", refused);
-		       ISFSArray accplayer = gamesInCreation.get(entry.getKey()).getAcceptedPlayers();
-		       for (int j=0; j<accplayer.size();j++){ //Sends the response to the joinned players.
-		       	parentEx.send("AcceptedRefused", rtn, users.get(accplayer.getUtfString(j)));
-		       }
-		       found =true;
 			}
+			else{
+				@SuppressWarnings("rawtypes")
+				Set set = gamesInCreation.entrySet();
+	
+				@SuppressWarnings("rawtypes")
+				Iterator iter = set.iterator();
+				boolean found = false;
+				while (iter.hasNext() && !found) { //Gets the keys of the hashmap. This keys are the user names
+				@SuppressWarnings({ "rawtypes" })
+				Map.Entry entry = (Map.Entry) iter.next();
+				if(gamesInCreation.get(entry.getKey()).getAcceptedPlayers().contains(player.getName())){
+					gamesInCreation.get(entry.getKey()).putRefused(player.getName());
+					
+					ISFSArray accepted = new SFSArray();
+					 accepted = gamesInCreation.get(entry.getKey()).getAcceptedPlayers();
+					
+					ISFSArray refused = new SFSArray();
+					 refused = gamesInCreation.get(entry.getKey()).getRefusedPlayers();
+					
+			       ISFSObject rtn = new SFSObject();
+			       rtn.putSFSArray("acceptedPlayers", accepted);
+			       rtn.putSFSArray("refusedPlayers", refused);
+			       ISFSArray accplayer = gamesInCreation.get(entry.getKey()).getAcceptedPlayers();
+			       for (int j=0; j<accplayer.size();j++){ //Sends the response to the joinned players.
+			       	parentEx.send("AcceptedRefused", rtn, users.get(accplayer.getUtfString(j)));
+			       }
+			       found =true;
+				}
+				}
 			}
+			
+			try{
+				Queue<User> queue1 = parentEx.getQueue1();
+				queue1.remove(player);
+			}catch(Exception e){};
+			try{
+				Queue<User[]> queue2 = parentEx.getQueue2();
+				Iterator<User[]> it=queue2.iterator();
+				boolean enc=false;
+				User[] arrayUsers=null;
+				int i;
+				while(it.hasNext()&&enc==false){
+					arrayUsers=it.next();
+						i=0;
+						while(i<arrayUsers.length&&enc==false){
+							enc= arrayUsers[i]==player;
+							i++;
+						}
+				}
+				if(enc==true){
+					if(arrayUsers[0]!=player)this.send("OutOfQueue", new SFSObject(), arrayUsers[0]);
+					if(arrayUsers[1]!=player)this.send("OutOfQueue", new SFSObject(), arrayUsers[1]);
+					queue2.remove(arrayUsers);		
+				}
+			}catch(Exception e){};
+			try{
+				Queue<User[]> queue3 = parentEx.getQueue3();
+				Iterator<User[]> it=queue3.iterator();
+				boolean enc=false;
+				User[] arrayUsers=null;
+				int i;
+				while(it.hasNext()&&enc==false){
+					arrayUsers=it.next();
+						i=0;
+						while(i<arrayUsers.length && enc==false){
+							enc= arrayUsers[i]==player;
+							i++;
+						}
+				}
+				if(enc==true){
+					if(arrayUsers[0]!=player)this.send("OutOfQueue", new SFSObject(), arrayUsers[0]);
+					if(arrayUsers[1]!=player)this.send("OutOfQueue", new SFSObject(), arrayUsers[1]);
+					if(arrayUsers[2]!=player)this.send("OutOfQueue", new SFSObject(), arrayUsers[2]);
+					queue3.remove(arrayUsers);
+				}
+			}catch(Exception e){};
 		}
-		
-		try{
-			Queue<User> queue1 = parentEx.getQueue1();
-			queue1.remove(player);
-		}catch(Exception e){};
-		try{
-			Queue<User[]> queue2 = parentEx.getQueue2();
-			Iterator<User[]> it=queue2.iterator();
-			boolean enc=false;
-			User[] arrayUsers=null;
-			int i;
-			while(it.hasNext()&&enc==false){
-				arrayUsers=it.next();
-					i=0;
-					while(i<arrayUsers.length&&enc==false){
-						enc= arrayUsers[i]==player;
-						i++;
-					}
-			}
-			if(enc==true){
-				if(arrayUsers[0]!=player)this.send("OutOfQueue", new SFSObject(), arrayUsers[0]);
-				if(arrayUsers[1]!=player)this.send("OutOfQueue", new SFSObject(), arrayUsers[1]);
-				queue2.remove(arrayUsers);		
-			}
-		}catch(Exception e){};
-		try{
-			Queue<User[]> queue3 = parentEx.getQueue3();
-			Iterator<User[]> it=queue3.iterator();
-			boolean enc=false;
-			User[] arrayUsers=null;
-			int i;
-			while(it.hasNext()&&enc==false){
-				arrayUsers=it.next();
-					i=0;
-					while(i<arrayUsers.length && enc==false){
-						enc= arrayUsers[i]==player;
-						i++;
-					}
-			}
-			if(enc==true){
-				if(arrayUsers[0]!=player)this.send("OutOfQueue", new SFSObject(), arrayUsers[0]);
-				if(arrayUsers[1]!=player)this.send("OutOfQueue", new SFSObject(), arrayUsers[1]);
-				if(arrayUsers[2]!=player)this.send("OutOfQueue", new SFSObject(), arrayUsers[2]);
-				queue3.remove(arrayUsers);
-			}
-		}catch(Exception e){};
 		/*Set<String> names= users.keySet(); //TODO tell friends he was disconected
 		Iterator<String> it=names.iterator();
 		if (it.hasNext()){				// here we tell to another user the disconection of this player (TO DO "tell" friends got disconnected)
