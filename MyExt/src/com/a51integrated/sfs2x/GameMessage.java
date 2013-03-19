5596aac8c54f8c1e9ee4c39e31f6050bf69998d6
Dani <danicr16@gmail.com>
2013-03-19 16:39:28 +0100

    GameMessage updated again, GetTime deleted.

--- MyExt/src/com/a51integrated/sfs2x/GameMessage.java
+++ MyExt/src/com/a51integrated/sfs2x/GameMessage.java
@@ -5,16 +5,12 @@ import java.util.Iterator;
 import com.smartfoxserver.v2.entities.Room;
 import com.smartfoxserver.v2.entities.User;
 import com.smartfoxserver.v2.entities.data.ISFSObject;
-import com.smartfoxserver.v2.entities.data.SFSObject;
 import com.smartfoxserver.v2.extensions.BaseClientRequestHandler;
 
 public class GameMessage extends BaseClientRequestHandler {
 
 	public void handleClientRequest(User player, ISFSObject params) {
 		MyExt parentEx = (MyExt) getParentExtension();
-		ISFSObject rtn = new SFSObject();
-		rtn.putUtfString("nada","1"); //Prepare the SFSObject which is going to be sent with the server time.
-		parentEx.send("hola", rtn, player);
 		Room room=player.getJoinedRooms().iterator().next(); //get the room of the player
 		Iterator<User> it=room.getPlayersList().iterator();
 		while (it.hasNext()){ // send it to all users of the room
