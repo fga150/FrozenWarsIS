346463f09c5e038321e616b741c8e883ccbe5f33
gonzalopericacho <gonzalopericacho@gmail.com>
2013-03-30 20:21:22 +0100

    updated getconnectedfriends(added database)
    commented the orther code in case you want to continue working without
    the database

--- MyExt/src/com/a51integrated/sfs2x/GetConnectedFriends.java
+++ MyExt/src/com/a51integrated/sfs2x/GetConnectedFriends.java
@@ -1,8 +1,10 @@
 package com.a51integrated.sfs2x;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 
 import com.smartfoxserver.v2.entities.User;
 import com.smartfoxserver.v2.entities.data.ISFSArray;
@@ -23,11 +25,9 @@ public class GetConnectedFriends extends BaseClientRequestHandler {
 		ISFSArray connectedFriends = new SFSArray();
 		
 		HashMap<String,User> users = parentEx.getUsers(); //Gets the hasmap which contains the connected users.
-		@SuppressWarnings("rawtypes")
-		Set set = users.entrySet();
 
-		@SuppressWarnings("rawtypes")
-		Iterator iter = set.iterator();
+		/*@SuppressWarnings("rawtypes")
+		Iterator iter = users.entrySet().iterator();
 
 		while (iter.hasNext()) { //Gets the keys of the hashmap. This keys are the user names
 		@SuppressWarnings("rawtypes")
@@ -39,17 +39,28 @@ public class GetConnectedFriends extends BaseClientRequestHandler {
 		}else 
 			if(!player.getName().equals(entry.getKey()) && !users.get(entry.getKey()).getLastJoinedRoom().isDynamic())
 			connectedFriends.addUtfString((String) entry.getKey());
+		}*/
+		try {
+			Connection connection = getParentExtension().getParentZone().getDBManager().getConnection();// catch the manager of the db
+			PreparedStatement stmt = connection.prepareStatement("SELECT friend FROM friends WHERE name=? AND status=?");
+		    stmt.setString(1, player.getName());
+		    stmt.setString(2, "c");
+			ResultSet res= stmt.executeQuery(); // send a query to know all friends with the mode said
+        	while (res.next()) {
+        		if (users.get(res.getString(1))!=null)
+        			if(gamesInCreation.containsKey(player.getName())){
+	        			if(!gamesInCreation.get(player.getName()).getAcceptedPlayers().contains(res.getString(1)) && !gamesInCreation.get(player.getName()).getWaitingPlayers().contains(res.getString(1))
+	        					&& !users.get(res.getString(1)).getLastJoinedRoom().isDynamic())
+	        			connectedFriends.addUtfString(res.getString(1));
+        			}else{ 
+        				if(!player.getName().equals(res.getString(1)) && !users.get(res.getString(1)).getLastJoinedRoom().isDynamic())
+        				connectedFriends.addUtfString(res.getString(1));
+        			}
+			}
+		}catch(Exception e){
+			
 		}
 		rtn.putSFSArray("ConnectedFriends", connectedFriends);
-		parentEx.send("GetConnectedFriends", rtn, player); //Sends the object which contains the connected users
-		
-		
-		
-		
-		
-		
-        
+		parentEx.send("GetConnectedFriends", rtn, player); //Sends the object which contains the connected users	      
 	}
-
-
 }
