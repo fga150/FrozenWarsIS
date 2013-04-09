e7baa669443d2735473882439eb3848498fd583a
Beatriz Gomez Carrero <betrizgomez283>
2013-04-09 19:37:05 +0200

    Comments in"LoginScreen" and changes in  FriendL
    Some comments has been added to LogInScreen.
    Also we can enter a friend's name in FriendListScreen so he/she could be
    added as a friend

--- FrozenWarsJava/src/Screens/FriendsListScreen.java
+++ FrozenWarsJava/src/Screens/FriendsListScreen.java
@@ -13,6 +13,7 @@ import Server.SmartFoxServer;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Input.TextInputListener;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
@@ -38,20 +39,6 @@ public class FriendsListScreen implements Screen{
 		return instance;
 	}
 	
-	private class InvitedInfo{
-		private String userName;
-		private String status;
-		public String getStatus() {
-			return status;
-		}
-		public String getUserName() {
-			return userName;
-		}
-		public InvitedInfo(String userName, String status) {
-			this.userName = userName;
-			this.status = status;
-		}
-	}	
 	    /** The gui cam. */
 	private OrthographicCamera guiCam;	
 	
@@ -63,10 +50,10 @@ public class FriendsListScreen implements Screen{
 	private SmartFoxServer sfsClient;
 	private BitmapFont font;
 	     
-    private BoundingBox MultiplayerClick;
-    
+    private BoundingBox MultiplayerClick; 
     private  BoundingBox backButtonClick;
     private  BoundingBox addFriendClick;
+	private BoundingBox userClick;
     
     // Settings
     private boolean externalPlayers;
@@ -83,154 +70,12 @@ public class FriendsListScreen implements Screen{
     private Vector<String> refusedPlayers;
     private Vector<String> waitingPlayers;
     
-    private Vector<InvitedInfo> drawPlayers;
-    
-    public void setAcceptedPlayers(Vector<String> acceptedPlayers) {
-		this.acceptedPlayers = acceptedPlayers;
-		drawPlayers = new Vector<InvitedInfo>();
-		for (int i = 0; i < acceptedPlayers.size(); i++) {
-			String name = acceptedPlayers.elementAt(i);
-			if (font.getBounds(name).width > 195){
-				int j = 5;
-				while (font.getBounds(name.substring(0, j)).width < 168) j++;
-				name = name.substring(0, j)+"...";
-			}
-			drawPlayers.add(new InvitedInfo(name, "Accepted"));
-		}
-		for (int i = 0; i < waitingPlayers.size(); i++) {
-			String name = waitingPlayers.elementAt(i);
-			if (font.getBounds(name).width > 195){
-				int j = 5;
-				while (font.getBounds(name.substring(0, j)).width < 168) j++;
-				name = name.substring(0, j)+"...";
-			}
-			drawPlayers.add(new InvitedInfo(name, "Waiting"));		
-		}
-		for (int i = 0; i < refusedPlayers.size(); i++) {
-			String name = refusedPlayers.elementAt(i);
-			if (font.getBounds(name).width > 195){
-				int j = 5;
-				while (font.getBounds(name.substring(0, j)).width < 168) j++;
-				name = name.substring(0, j)+"...";
-			}
-			drawPlayers.add(new InvitedInfo(name, "Cancelled"));
-		}
-		
-	}
-    
-    public void setRefusedPlayers(Vector<String> refusedPlayers) {
-		this.refusedPlayers = refusedPlayers;
-		drawPlayers = new Vector<InvitedInfo>();
-		for (int i = 0; i < acceptedPlayers.size(); i++) {
-			String name = acceptedPlayers.elementAt(i);
-			if (font.getBounds(name).width > 195){
-				int j = 5;
-				while (font.getBounds(name.substring(0, j)).width < 168) j++;
-				name = name.substring(0, j)+"...";
-			}
-			drawPlayers.add(new InvitedInfo(name, "Accepted"));
-		}
-		for (int i = 0; i < waitingPlayers.size(); i++) {
-			String name = waitingPlayers.elementAt(i);
-			if (font.getBounds(name).width > 195){
-				int j = 5;
-				while (font.getBounds(name.substring(0, j)).width < 168) j++;
-				name = name.substring(0, j)+"...";
-			}
-			drawPlayers.add(new InvitedInfo(name, "Waiting"));		
-		}
-		for (int i = 0; i < refusedPlayers.size(); i++) {
-			String name = refusedPlayers.elementAt(i);
-			if (font.getBounds(name).width > 195){
-				int j = 5;
-				while (font.getBounds(name.substring(0, j)).width < 168) j++;
-				name = name.substring(0, j)+"...";
-			}
-			drawPlayers.add(new InvitedInfo(name, "Cancelled"));
-		}
-	}
-    
-    public void setWaitingPlayers(Vector<String> waitingPlayers) {
-		this.waitingPlayers = waitingPlayers;
-		drawPlayers = new Vector<InvitedInfo>();
-		for (int i = 0; i < acceptedPlayers.size(); i++) {
-			String name = acceptedPlayers.elementAt(i);
-			if (font.getBounds(name).width > 195){
-				int j = 5;
-				while (font.getBounds(name.substring(0, j)).width < 168) j++;
-				name = name.substring(0, j)+"...";
-			}
-			drawPlayers.add(new InvitedInfo(name, "Accepted"));
-		}
-		for (int i = 0; i < waitingPlayers.size(); i++) {
-			String name = waitingPlayers.elementAt(i);
-			if (font.getBounds(name).width > 195){
-				int j = 5;
-				while (font.getBounds(name.substring(0, j)).width < 168) j++;
-				name = name.substring(0, j)+"...";
-			}
-			drawPlayers.add(new InvitedInfo(name, "Waiting"));		
-		}
-		for (int i = 0; i < refusedPlayers.size(); i++) {
-			String name = refusedPlayers.elementAt(i);
-			if (font.getBounds(name).width > 195){
-				int j = 5;
-				while (font.getBounds(name.substring(0, j)).width < 168) j++;
-				name = name.substring(0, j)+"...";
-			}
-			drawPlayers.add(new InvitedInfo(name, "Cancelled"));
-		}
-	}
-    
-    public void setExternalPlayers(boolean externalPlayers) {
-		this.externalPlayers = externalPlayers;
-	}
-
-	public void setGameMode(int gameMode) {
-		this.gameMode = gameMode;
-	}
-	
-	public boolean amIAdmin(){
-		return myName.equals(gameAdmin);
-	}
-	
-	public void setEmpiezaPartida(boolean b) {
-		empiezaPartida = b;
-	}
-	
-	public void setMyName(String myName) {
-		this.myName = myName;
-		acceptedPlayers.add(myName);
-		
-		String name = myName;
-		if (font.getBounds(name).width > 195){
-			int j = 5;
-			while (font.getBounds(name.substring(0, j)).width < 168) j++;
-			name = name.substring(0, j)+"...";
-		}
-		drawPlayers.add(new InvitedInfo(name, "Accepted"));
-	
-		gameAdmin = myName;
-	}
-	
-	public void setGameAdmin(String gameAdmin){
-		this.gameAdmin = gameAdmin;
-	}
-	
-
-	public String getGameAdmin() {
-		return gameAdmin;
-	}
-	
-	public void setInQueue(boolean inQueue) {
-		this.inQueue = inQueue;
-	}
+	private String user;	
+	private long time;
+	private boolean infoPressed;
     
 
-	public boolean isInQueue() {
-		return inQueue;
-	}
-	
+
     public FriendsListScreen() {
 		instance = this;
     	this.game = LaunchFrozenWars.getGame();
@@ -245,35 +90,19 @@ public class FriendsListScreen implements Screen{
 	    acceptedPlayers = new Vector<String>();
 	    refusedPlayers = new Vector<String>();
 	    waitingPlayers = new Vector<String>();
-	    drawPlayers = new Vector<InvitedInfo>();
+	    
+	    infoPressed = false;
+	    user = "";
+	    this.time = System.nanoTime();
 	    
 	    invitedScroll = 0;
 	    
 	    sfsClient = SmartFoxServer.getInstance();
-	    
-	    /*Gdx.input.getTextInput(new TextInputListener() {
-	         public void input(String text) {
-	        	 MultiplayerScreen.getInstance().setMyName(text);
-	        	 sfsClient.conectaSala(text,"");//TODO pass also the password
-	         }
-	
-	         public void canceled() {
-	        	 String user = "user".concat(Long.toString(Math.round(Math.random()*1000)));
-	        	 MultiplayerScreen.getInstance().setMyName(user);
-	        	 sfsClient.conectaSala(user,""); //TODO pass also the password
-	         }
-	    }, "Enter user: ","");
-
-	    */
 
 	    backButtonClick = new BoundingBox(new Vector3(320,20,0), new Vector3(560,60,0));
-	    addFriendClick = new BoundingBox(new Vector3(395,118,0), new Vector3(439,155,0));
+	    addFriendClick = new BoundingBox(new Vector3(615,110,0), new Vector3(660,145,0));
 	    MultiplayerClick = new BoundingBox(new Vector3(30,540,0), new Vector3(510,605,0));
-	      
-	    inQueue = false;
-	    empiezaPartida = false;
-	    externalPlayers = true;
-	    gameMode = 0;
+	    userClick = new BoundingBox(new Vector3(310,105,0), new Vector3(605,140,0));
 	}
 
 	
@@ -292,17 +121,6 @@ public class FriendsListScreen implements Screen{
 	public void pause() {
 	}
 
-	public void creaPartida(){
-		MatchManager manager = new MatchManager(sfsClient);
-		sfsClient.addManager(manager);
-		GameScreen gameScreen = new GameScreen(game,manager);
-		manager.setGameScreen(gameScreen);
-		game.setScreen(gameScreen);
-		System.out.println(sfsClient.getMyPlayerId());
-		if(sfsClient.getMyPlayerId()==1){     //PROBAR CUANDO DANI SUBA CAMBIOS DEL SERVER
-			sfsClient.sendAsign();
-		}
-	}
 	
 	@Override
 	public void render(float arg0) {
@@ -317,10 +135,18 @@ public class FriendsListScreen implements Screen{
       			game.setScreen(InitialScreen.getInstance());
       		 } else if (MultiplayerClick.contains(touchPoint)){
       			this.game.setScreen(MultiplayerScreen.getInstance());
-      		}
+      		 } else if (userClick.contains(touchPoint)){
+      			Gdx.input.setOnscreenKeyboardVisible(true);
+      			this.infoPressed = true;
+      			System.out.println("user");
+      		 } else if (addFriendClick.contains(touchPoint)){
+       			System.out.println("AddFriend");
+       		 } else {
+      			this.infoPressed = false;
+      		 }
 		}
 		
-		if (empiezaPartida) creaPartida();
+		
 		//crear solamente un batcher por pantalla y eliminarlo cuando no se use
 	
       		GL10 gl = Gdx.graphics.getGL10(); //referencia a OpenGL 1.0
@@ -349,57 +175,32 @@ public class FriendsListScreen implements Screen{
             
             batcher.draw(Assets.addFriend, 290, 90); 
 
-            	          
+            font.drawWrapped(batcher, user, 320,140, 260);
+ 
             batcher.end();
             
-            ConfirmScreen.getInstance().createConfirmIfNeeded();
-            AcceptScreen.getInstance().createAcceptIfNeeded();
-
+            if ( this.infoPressed && ((System.nanoTime() - this.time) > 135000000)){
+            	this.completeMessagePc();
+            	this.time = System.nanoTime();
+            }
 	}
 
-	private void drawInvited() {
-		batcher.draw(Assets.list, 630, 180); 
-		batcher.draw(Assets.playersText, 720, 407); 
-		
-		for (int i = 0; i < Math.min(drawPlayers.size(), 5); i++){
-			String name = drawPlayers.elementAt(i+invitedScroll).getUserName();
-			
-			font.draw(batcher, name, 700,(402-45*i));
-			if (drawPlayers.elementAt(i+invitedScroll).getStatus().equals("Accepted")) 
-				batcher.draw(Assets.statusTick, 650, (367-45*i));
-			else if (drawPlayers.elementAt(i+invitedScroll).getStatus().equals("Waiting")) 
-				batcher.draw(Assets.statusInterrogation, 650, (367-45*i));
-			else if (drawPlayers.elementAt(i+invitedScroll).getStatus().equals("Cancelled"))
-				batcher.draw(Assets.statusCancel, 650, (367-45*i));
-		}
-	}
-
-
-	public void setDefault() {
-	    acceptedPlayers.clear();
-	    refusedPlayers.clear();
-	    waitingPlayers.clear();
-	    drawPlayers.clear();
-	    
-	    invitedScroll = 0;
-	    externalPlayers = true;
-	    gameMode = 0;
-	    inQueue = false;
-	    
-		acceptedPlayers.add(myName);
-		
-		String name = myName;
-		if (font.getBounds(name).width > 195){
-			int j = 5;
-			while (font.getBounds(name.substring(0, j)).width < 168) j++;
-			name = name.substring(0, j)+"...";
-		}
-		drawPlayers.add(new InvitedInfo(name, "Accepted"));
 	
-		gameAdmin = myName;
-	}
-	
+	private void completeMessagePc(){
+		char aux = 0;
+		
+		if(Gdx.input.isKeyPressed(Keys.ANY_KEY)){
+			aux = ScreensKeyboard.keyPc();
+		}
 
+        if (aux != 0){
+	        user += aux;	        
+        }  
+        
+        if (ScreensKeyboard.delete() && user.length()>0){
+	        user = (String)user.subSequence(0, user.length()-1);    
+        }
+	}
 	
 	
 	@Override
--- FrozenWarsJava/src/Screens/LogInScreen.java
+++ FrozenWarsJava/src/Screens/LogInScreen.java
@@ -1,7 +1,6 @@
 package Screens;
 
 import Application.Assets;
-import Application.GameSettings;
 import Application.LaunchFrozenWars;
 
 import Server.SmartFoxServer;
@@ -41,8 +40,7 @@ public class LogInScreen implements Screen {
 	private String pass;
 	private String passShown;	
 	private long time;
-	
-/*TODO : Poner singleton */
+
 	
 	public LogInScreen() {
 		this.game = LaunchFrozenWars.getGame();
@@ -51,17 +49,16 @@ public class LogInScreen implements Screen {
 		font = new BitmapFont(Gdx.files.internal("data/first.fnt"), Gdx.files.internal("data/first.png"), false);;
 		
 		this.user = this.pass = this.passShown = "";
-
-		
+	
 		guiCam = new OrthographicCamera(1024,630);
 		guiCam.position.set(512,315,0);
-
-		
-		font = new BitmapFont(Gdx.files.internal("data/first.fnt"), Gdx.files.internal("data/first.png"), false);;
+	
+		font = new BitmapFont(Gdx.files.internal("data/first.fnt"), Gdx.files.internal("data/first.png"), false);
 
 	    batcher = new SpriteBatch();
 	    touchPoint = new Vector3();
 
+	    //Bounding boxes to check if some component's screen has been touched
 	    userClick = new BoundingBox(new Vector3(440,395,0), new Vector3(670,440,0));
 	    passClick = new BoundingBox(new Vector3(435,325,0), new Vector3(700,370,0));      
 	    okClick = new BoundingBox(new Vector3(335,240,0), new Vector3(500,300,0));  
@@ -87,46 +84,40 @@ public class LogInScreen implements Screen {
 
 	@Override
 	public void render(float arg0) { 
-      //detectamos si se ha tocado la pantalla
-      if (Gdx.input.justTouched()){
+      
+      if (Gdx.input.justTouched()){ 
       		guiCam.unproject(touchPoint.set(Gdx.input.getX(),Gdx.input.getY(),0));
-      		System.out.println(Integer.toString((int)touchPoint.x).concat(",").concat(Integer.toString((int)touchPoint.y)));
-
-      		//compruebo si he tocado yes 
-      		if (userClick.contains(touchPoint)){
+  		
+      		if (userClick.contains(touchPoint)){ //Check if we have clicked on user's square 
       			Gdx.input.setOnscreenKeyboardVisible(true);
       			this.infoPressed = 1;
-      			System.out.println("user");
-      		} else if(passClick.contains(touchPoint)){ //compruebo si he tocado no
+      		} else if(passClick.contains(touchPoint)){ //Check if we have clicked on pass's square (primary pass)
       			Gdx.input.setOnscreenKeyboardVisible(true);
       			this.infoPressed = 2;
-      			System.out.println("pass");
-      		} else if(okClick.contains(touchPoint)){ //compruebo si he tocado no
-      			System.out.println("ok");
+      		} else if(okClick.contains(touchPoint)){ //Check if we have clicked on "ok" button 
       			sfsClient.conectaSala(user, pass);
-      		} else if(backClick.contains(touchPoint)){ //compruebo si he tocado no
+      		} else if(backClick.contains(touchPoint)){ //Check if we have clicked on "back" button 
       			game.setScreen(LogSignScreen.getInstance());
       			System.out.println("back");
-      		} else{
+      		} else{ //We haven't clicked on any component
       			this.infoPressed = 0;
       		}
       }
-      //crear solamente un batcher por pantalla y eliminarlo cuando no se use
-        	GL10 gl = Gdx.graphics.getGL10(); //referencia a OpenGL 1.0
+      //One batcher in each screen. We should remove it when we don't need it anymore
+        	GL10 gl = Gdx.graphics.getGL10(); //references to OpenGL 1.0
             gl.glClearColor(0,1,0,1);
             gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
  
             guiCam.update();
             batcher.setProjectionMatrix(guiCam.combined);
              
-            //Dibujando el Background
+           
             batcher.disableBlending();
-            //se elimina graficamente la transparencia ya que es un fondo
             batcher.begin();
-            batcher.draw(Assets.backGrey,0,0, 1024, 630);
+            batcher.draw(Assets.backGrey,0,0, 1024, 630);  //Drawning Background
             batcher.end();
 
-          //Dibujando elementos en pantalla activamos el Blending
+          //Dranwing elements on the screen
             batcher.enableBlending();
             batcher.begin();    
             batcher.draw(Assets.logInWindow, 300, 200);
@@ -134,9 +125,8 @@ public class LogInScreen implements Screen {
             font.drawWrapped(batcher, user, 450, 435, 240);
             font.drawWrapped(batcher, passShown, 450, 365, 240);
             batcher.end();	
-
-            
-            if ( this.infoPressed !=0 && ((System.nanoTime() - this.time) > 135000000)){
+ 
+            if ( this.infoPressed !=0 && ((System.nanoTime() - this.time) > 135000000)){ //We check if some info's square has been pressed
             	this.completeMessagePc();
             	this.time = System.nanoTime();
             }
@@ -162,23 +152,21 @@ public class LogInScreen implements Screen {
 	private void completeMessagePc(){
 		char aux = 0;
 		
-		if(Gdx.input.isKeyPressed(Keys.ANY_KEY)){
+		if(Gdx.input.isKeyPressed(Keys.ANY_KEY)){ //We check if some key has been pressed
 			  aux = ScreensKeyboard.keyPc();
 		}
 
-        if (aux != 0){
-	        if (this.infoPressed == 1){
-	        	user += aux;
-	        	System.out.println("user: " + user);
-	        } else if (this.infoPressed == 2){
-	        	pass += aux;
-	        	passShown +='$';
-	        	System.out.println("pass: " + pass);
+        if (aux != 0){ //aux would be !=0 if a key has been pressed
+	        if (this.infoPressed == 1){ //If we are writing user's name
+	        	user += aux;    //We add the char pressed
+	        } else if (this.infoPressed == 2){  //If we are writing pass
+	        	pass += aux; //We add the char pressed
+	        	passShown +='$'; 
 	        } 
         }  
         
-        if (ScreensKeyboard.delete()){
-        	if (this.infoPressed == 1 && user.length()>0){
+        if (ScreensKeyboard.delete()){ //If we have pressed delete key, we have to check what info are we editing
+        	if (this.infoPressed == 1 && user.length()>0){ 
 	        	user = (String)user.subSequence(0, user.length()-1);
 	        } else if (this.infoPressed == 2 && pass.length()>0){
 	        	pass = (String)pass.subSequence(0, pass.length()-1);
