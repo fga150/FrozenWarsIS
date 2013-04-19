7b9ac009c58d7f3f5cb82fcbf7fced49088c2813
Daniel López Carreras <danilc91@gmail.com>
2013-04-19 16:01:19 +0200

    Some changes for using mobile buttons
    I have been investigating how to use buttons like back, or setting.

--- FrozenWarsJava/src/Application/LaunchFrozenWars.java
+++ FrozenWarsJava/src/Application/LaunchFrozenWars.java
@@ -6,10 +6,13 @@ import Screens.MultiplayerScreen;
 import Server.SmartFoxServer;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 
 
-public class LaunchFrozenWars extends Game {
+public class LaunchFrozenWars extends Game implements InputProcessor {
 	private static Game instance;
 	
 	public static Game getGame(){
@@ -19,6 +22,9 @@ public class LaunchFrozenWars extends Game {
 	private Screen loadScreen;
 	@Override
 	public void create() {
+		Gdx.input.setInputProcessor(this);
+		Gdx.input.setCatchBackKey(true);
+	    Gdx.input.setCatchMenuKey(true);
 		Assets.load();
 	    instance = this;
 		loadScreen = new LoadScreen();
@@ -34,6 +40,67 @@ public class LaunchFrozenWars extends Game {
 		GameSettings.getInstance().saveSettings();
 		instance = null;
 		System.exit(0);
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+	       if(keycode == Keys.BACK){
+	             // Do back button handling (show pause menu?)
+	    	 /*  if(InitialScreen.getInstance().isMultiplayerOn() && this.getScreen()==InviteScreen.getInstance()){
+	   			loadScreen = MultiplayerScreen.getInstance();
+	   			setScreen(loadScreen);
+	    	   }
+	   			else if(this.getScreen()==HelpScreen.getInstance()){
+		   			loadScreen = IndexScreen.getInstance();
+		   			setScreen(loadScreen);
+	   			}
+	   			else {*/
+		   			setScreen(InitialScreen.getInstance());
+	   			//}
+	       }
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int arg0, int arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchMoved(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int arg0, int arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
