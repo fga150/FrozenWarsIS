30738969c1c1bd145c8aaaab64588adcd7adcc6d
Dani <danicr16@gmail.com>
2013-05-08 18:59:23 +0200

    Revert "Response in extract from queue"
    This reverts commit e2841ba1da21ef40d79fa614c3529703fb166bfc.

--- MyExt/src/com/a51integrated/sfs2x/ExtractUsersHandler.java
+++ MyExt/src/com/a51integrated/sfs2x/ExtractUsersHandler.java
@@ -5,19 +5,23 @@ import java.util.Queue;
 
 import com.smartfoxserver.v2.entities.User;
 import com.smartfoxserver.v2.entities.data.ISFSObject;
-import com.smartfoxserver.v2.entities.data.SFSObject;
 import com.smartfoxserver.v2.extensions.BaseClientRequestHandler;
 
 public class ExtractUsersHandler extends BaseClientRequestHandler {
 
 	public void handleClientRequest(User player, ISFSObject params) {
+
 		MyExt parentEx = (MyExt) getParentExtension();
-		try{
-			Queue<User> queue1 = parentEx.getQueue1();
+		int numJug=params.getInt("numJugadores");
+		int mode = params.getInt("mode");
+		
+		if(mode == 0){
+		if(numJug==1){
+			Queue<User> queue1 = parentEx.getQueue01();
 			queue1.remove(player);
-		}catch(Exception e){};
-		try{
-			Queue<User[]> queue2 = parentEx.getQueue2();
+		}
+		else if(numJug==2){
+			Queue<User[]> queue2 = parentEx.getQueue02();
 			Iterator<User[]> it=queue2.iterator();
 			boolean enc=false;
 			User[] arrayUsers=null;
@@ -30,16 +34,10 @@ public class ExtractUsersHandler extends BaseClientRequestHandler {
 						i++;
 					}
 			}
-			if(enc==true){
-				ISFSObject rtn = new SFSObject();
-				rtn.putUtfString("playerName", player.getName());
-				if(arrayUsers[0]!=player)this.send("OutOfQueue", rtn, arrayUsers[0]);
-				if(arrayUsers[1]!=player)this.send("OutOfQueue", rtn, arrayUsers[1]);
-				queue2.remove(arrayUsers);		
-			}
-		}catch(Exception e){};
-		try{
-			Queue<User[]> queue3 = parentEx.getQueue3();
+			if(enc==true)queue2.remove(arrayUsers);
+		}
+		else if(numJug==3){
+			Queue<User[]> queue3 = parentEx.getQueue03();
 			Iterator<User[]> it=queue3.iterator();
 			boolean enc=false;
 			User[] arrayUsers=null;
@@ -47,20 +45,173 @@ public class ExtractUsersHandler extends BaseClientRequestHandler {
 			while(it.hasNext()&&enc==false){
 				arrayUsers=it.next();
 					i=0;
-					while(i<arrayUsers.length && enc==false){
+					while(i<arrayUsers.length&&enc==false){
 						enc= arrayUsers[i]==player;
 						i++;
 					}
 			}
-			if(enc==true){
-				ISFSObject rtn = new SFSObject();
-				rtn.putUtfString("playerName", player.getName());
-				if(arrayUsers[0]!=player)this.send("OutOfQueue", rtn, arrayUsers[0]);
-				if(arrayUsers[1]!=player)this.send("OutOfQueue", rtn, arrayUsers[1]);
-				if(arrayUsers[2]!=player)this.send("OutOfQueue", rtn, arrayUsers[2]);
-				queue3.remove(arrayUsers);
+			if(enc==true)queue3.remove(arrayUsers);
+		}
+		
+	}
+		
+		else if (mode == 1){
+			if(numJug==1){
+				Queue<User> queue1 = parentEx.getQueue11();
+				queue1.remove(player);
 			}
-		}catch(Exception e){};
+			else if(numJug==2){
+				Queue<User[]> queue2 = parentEx.getQueue12();
+				Iterator<User[]> it=queue2.iterator();
+				boolean enc=false;
+				User[] arrayUsers=null;
+				int i;
+				while(it.hasNext()&&enc==false){
+					arrayUsers=it.next();
+						i=0;
+						while(i<arrayUsers.length&&enc==false){
+							enc= arrayUsers[i]==player;
+							i++;
+						}
+				}
+				if(enc==true)queue2.remove(arrayUsers);
+			}
+			else if(numJug==3){
+				Queue<User[]> queue3 = parentEx.getQueue13();
+				Iterator<User[]> it=queue3.iterator();
+				boolean enc=false;
+				User[] arrayUsers=null;
+				int i;
+				while(it.hasNext()&&enc==false){
+					arrayUsers=it.next();
+						i=0;
+						while(i<arrayUsers.length&&enc==false){
+							enc= arrayUsers[i]==player;
+							i++;
+						}
+				}
+				if(enc==true)queue3.remove(arrayUsers);
+			}
+		}
+		
+		else if (mode == 2){
+			if(numJug==1){
+				Queue<User> queue1 = parentEx.getQueue21();
+				queue1.remove(player);
+			}
+			else if(numJug==2){
+				Queue<User[]> queue2 = parentEx.getQueue22();
+				Iterator<User[]> it=queue2.iterator();
+				boolean enc=false;
+				User[] arrayUsers=null;
+				int i;
+				while(it.hasNext()&&enc==false){
+					arrayUsers=it.next();
+						i=0;
+						while(i<arrayUsers.length&&enc==false){
+							enc= arrayUsers[i]==player;
+							i++;
+						}
+				}
+				if(enc==true)queue2.remove(arrayUsers);
+			}
+			else if(numJug==3){
+				Queue<User[]> queue3 = parentEx.getQueue23();
+				Iterator<User[]> it=queue3.iterator();
+				boolean enc=false;
+				User[] arrayUsers=null;
+				int i;
+				while(it.hasNext()&&enc==false){
+					arrayUsers=it.next();
+						i=0;
+						while(i<arrayUsers.length&&enc==false){
+							enc= arrayUsers[i]==player;
+							i++;
+						}
+				}
+				if(enc==true)queue3.remove(arrayUsers);
+			}
+		}
+		
+		else if (mode == 3){
+			
+			if(numJug==1){
+				Queue<User> queue1 = parentEx.getQueue31();
+				queue1.remove(player);
+			}
+			else if(numJug==2){
+				Queue<User[]> queue2 = parentEx.getQueue32();
+				Iterator<User[]> it=queue2.iterator();
+				boolean enc=false;
+				User[] arrayUsers=null;
+				int i;
+				while(it.hasNext()&&enc==false){
+					arrayUsers=it.next();
+						i=0;
+						while(i<arrayUsers.length&&enc==false){
+							enc= arrayUsers[i]==player;
+							i++;
+						}
+				}
+				if(enc==true)queue2.remove(arrayUsers);
+			}
+			else if(numJug==3){
+				Queue<User[]> queue3 = parentEx.getQueue33();
+				Iterator<User[]> it=queue3.iterator();
+				boolean enc=false;
+				User[] arrayUsers=null;
+				int i;
+				while(it.hasNext()&&enc==false){
+					arrayUsers=it.next();
+						i=0;
+						while(i<arrayUsers.length&&enc==false){
+							enc= arrayUsers[i]==player;
+							i++;
+						}
+				}
+				if(enc==true)queue3.remove(arrayUsers);
+			}
+		}
+		
+		else if (mode == 4){
+			
+			if(numJug==1){
+				Queue<User> queue1 = parentEx.getQueue41();
+				queue1.remove(player);
+			}
+			else if(numJug==2){
+				Queue<User[]> queue2 = parentEx.getQueue42();
+				Iterator<User[]> it=queue2.iterator();
+				boolean enc=false;
+				User[] arrayUsers=null;
+				int i;
+				while(it.hasNext()&&enc==false){
+					arrayUsers=it.next();
+						i=0;
+						while(i<arrayUsers.length&&enc==false){
+							enc= arrayUsers[i]==player;
+							i++;
+						}
+				}
+				if(enc==true)queue2.remove(arrayUsers);
+			}
+			else if(numJug==3){
+				Queue<User[]> queue3 = parentEx.getQueue43();
+				Iterator<User[]> it=queue3.iterator();
+				boolean enc=false;
+				User[] arrayUsers=null;
+				int i;
+				while(it.hasNext()&&enc==false){
+					arrayUsers=it.next();
+						i=0;
+						while(i<arrayUsers.length&&enc==false){
+							enc= arrayUsers[i]==player;
+							i++;
+						}
+				}
+				if(enc==true)queue3.remove(arrayUsers);
+			}
+		}
 
 
 	}
